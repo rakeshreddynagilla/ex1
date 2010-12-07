5d2013c2f803982be1508a5a7e7f3f742d24fe0b
rakeshreddynagilla <rakesh@loyaltybuilder2.(none)>
2010-12-07 12:54:17 -0600

    first push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExOne::Application.config.secret_token = '207cdd85b52e09cba34e77ac495845d43a2b83bb058749337fc51b22f6c4b59cbdfcd5625aad8103b01a65477929d39760e1358a6775c87570bc4a6e29e4ee07'
